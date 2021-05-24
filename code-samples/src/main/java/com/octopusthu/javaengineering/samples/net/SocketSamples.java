722f17d3d09272f805955da58a263e6e1f760878
figozhang <figozhang@tencent.com>
2021-05-24 20:52:15 +0800

    wip

--- code-samples/src/main/java/com/octopusthu/javaengineering/samples/net/SocketSamples.java
+++ code-samples/src/main/java/com/octopusthu/javaengineering/samples/net/SocketSamples.java
@@ -1,7 +1,87 @@
 package com.octopusthu.javaengineering.samples.net;
 
+import org.springframework.util.Assert;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
+ * 使用 Java Socket 实现简单的基于 TCP 的服务器/客户端。
+ *
  * @author octopusthu@gmail.com
  */
 public class SocketSamples {
+
+    static class HelloWorldServer {
+        private ServerSocket serverSocket;
+        private Socket clientSocket;
+        private PrintWriter out;
+        private BufferedReader in;
+
+        static int minPort = 1000;
+        static int maxPort = 1999;
+
+        public void start(int port) throws IOException {
+            Assert.isTrue(port >= minPort && port <= maxPort,
+                "Only port from " + minPort + " to " + maxPort + " is supported!");
+            serverSocket = new ServerSocket(port);
+
+            System.out.println("Server starts to listen on port " + port + "...");
+            clientSocket = serverSocket.accept();
+
+            String expectedClientMessage = "Hello, server!";
+            String expectedServerMessage = "Hello, client!";
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String actualClientMessage = in.readLine();
+            System.out.println("Server received message: " + actualClientMessage);
+            String actualServerMessage = expectedClientMessage.equals(actualClientMessage) ?
+                expectedServerMessage : "Unexpected message from client: " + actualClientMessage;
+            out.println(actualServerMessage);
+            System.out.println("Server sent message: " + actualServerMessage);
+
+            stop();
+        }
+
+        public void stop() throws IOException {
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+            System.out.println("Server stopped.");
+        }
+
+        public static void main(String[] args) throws IOException {
+            HelloWorldServer server = new HelloWorldServer();
+            server.start(1234);
+        }
+    }
+
+    static class HelloWorldClient {
+        private Socket clientSocket;
+        private PrintWriter out;
+        private BufferedReader in;
+
+        public void startConnection(String ip, int port) throws IOException {
+            clientSocket = new Socket(ip, port);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        }
+
+        public String sendMessage(String msg) throws IOException {
+            out.println(msg);
+            return in.readLine();
+        }
+
+        public void stopConnection() throws IOException {
+            in.close();
+            out.close();
+            clientSocket.close();
+        }
+    }
+
 }
--- docs/课程素材/第14周 - 工程实践第五讲 - Java Web 开发.md
+++ docs/课程素材/第14周 - 工程实践第五讲 - Java Web 开发.md
@@ -4,25 +4,8 @@
 
 ## 课后练习 (大作业)
 
-1. 基于以下业务规则编写 Team 和 TeamMember 类的单元测试，并完成对类的修改。
-   1. 每支球队有且仅有一名队长。
-   1. 每名球队成员必须有号码，号码不能小于1号或大于99号，不同球员的号码不能重复。
+1. 在本地成功编译 SocketSamples 并运行 HelloWorldServer 和 SocketSamplesTests 。
 
-1. 完成 MySQL 数据库的安装、配置和使用
-   1. 在本地安装 MySQL Community Server 最新版。
-   1. 通过 IntelliJ IDEA 的 Database 工具，使用 "root" 用户连接本地 MySQL 数据库。
-   1. 创建数据库 "tournament"
-   1. 运行脚本 "V1_0_0__Schema.sql" ，初始化数据表
-   1. 创建数据库用户 "tournament" ，将用户 Host 设为 "localhost" ，并为用户赋予增、删、改、查 "tournament" 数据库全部数据表的权限
-   1. 通过 IntelliJ IDEA 的 Database 工具，使用 "tournament" 用户连接本地 MySQL 数据库。
-   1. 运行脚本 "V1_0_0__Init_Data.sql" ，初始化数据
-
-1. 根据自己的大作业选题，设计数据库表结构，并在 MySQL 数据库中创建相应的库、表、用户和初始化数据。
-
-1. 使用 JDBC 操作关系型数据库实现大作业项目中至少一个领域的领域服务的实现。
-
-1. 使用 Spring JdbcTemplate 操作关系型数据库实现大作业项目中至少一个领域的领域服务的实现。
-
-1. 使用 Spring Data JPA 操作关系型数据库实现大作业项目中至少一个领域的领域服务的实现。
+1. 【选做】SocketSamples 中的服务端 Socket 服务器只能处理一个客户端连接。请你实现一个新的服务端 Socket ，能够同时处理多个客户端连接。
 
 1. 在下一讲上课前一天将自己的 user/xxx 分支推送到远程代码仓库中，供助教检查。
